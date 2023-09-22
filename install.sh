ed0a15d56055814b59090b4a0c39a6b9eb84d463
Txus Bach <me@txus.io>
2023-09-22 12:16:01 +0200

    Add tmux conf

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 source "$HOME/.cargo/env"
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply txus
