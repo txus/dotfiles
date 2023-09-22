7ff73f0f765ecfde618457edd525b2642b91e2bf
Txus Bach <me@txus.io>
2023-09-22 12:18:15 +0200

    verbose

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo 'Installing rust...'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 source "$HOME/.cargo/env"
+
+echo 'Installing LunarVim...'
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+
+echo 'Installing TPM...'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+echo 'Installing Chezmoi...'
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply txus
