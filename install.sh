0e8612e4935528d907fc2081deeb98596a5cd37a
Txus Bach <me@txus.io>
2023-09-22 12:09:31 +0200

    Install rust before lunarvim

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+source "$HOME/.cargo/env"
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply txus
