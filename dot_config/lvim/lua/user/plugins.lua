e578b4297078df39ce76938cda10133864ee97a3
Txus <me@txus.io>
2025-03-30 15:12:40 +0200

    basic lunarvim

---
+++ dot_config/lvim/lua/user/plugins.lua
@@ -0,0 +1,76 @@
+lvim.plugins = {
+  { 'lunarvim/lunar.nvim' },
+  { "morhetz/gruvbox" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/sonokai" },
+  { "sainnhe/edge" },
+  { "lunarvim/horizon.nvim" },
+  { "tomasr/molokai" },
+  { "ayu-theme/ayu-vim" },
+
+  { "folke/trouble.nvim", cmd = "TroubleToggle" },
+
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end
+  },
+
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("persistence").setup({
+        dir = vim.fn.expand(vim.fn.stdpath "state" .. "/sessions/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" }
+      })
+    end
+  },
+
+  { "christoomey/vim-tmux-navigator" },
+  { "tpope/vim-surround" },
+  { "felipec/vim-sanegx", event = "BufRead" },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+
+  { "tpope/vim-repeat" },
+  { "ThePrimeagen/harpoon" },
+  {
+    "phaazon/hop.nvim",
+    config = function()
+      require('hop').setup()
+    end
+  },
+
+  {
+    'nvim-telescope/telescope-frecency.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'kkharji/sqlite.lua' }
+  },
+
+  {
+    'AckslD/nvim-trevJ.lua',
+    config = 'require("trevj").setup()',
+    init = function()
+      vim.keymap.set('n', '<leader>j', function()
+        require('trevj').format_at_cursor()
+      end)
+    end
+  },
+
+  -- python
+  "ChristianChiarulli/swenv.nvim",
+  "stevearc/dressing.nvim",
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+}
+
+lvim.builtin.telescope.on_config_done = function(telescope)
+  pcall(telescope.load_extension, "frecency")
+end
--- dot_config/nvim/lua/plugins/better_escape.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { -- further customize the options set by the community
-    "better-escape.nvim",
-    opts = {
-      mapping = { "uf", "jj", "jk", "kk" },
-    },
-  },
-}
