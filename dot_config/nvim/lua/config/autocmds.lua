a285b59d7aee5ba4000e4a0b6241d6cdd984a625
Txus <me@txus.io>
2025-12-06 21:16:56 +0100

    dot config

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat({ "cpp" }, true)
+set_autoformat({ "python" }, false)
+set_autoformat({ "yaml" }, false)
+set_autoformat({ "json" }, false)
